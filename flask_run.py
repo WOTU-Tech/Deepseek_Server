676c69cf1f6ef0f5e24714630e9d1bf62ad34a0a
Wang <xinan.wang@7-11.com>
2025-02-06 17:12:47 -0600

    flask run

--- flask_run.py
+++ flask_run.py
@@ -1,21 +1,29 @@
 from flask import Flask, render_template, request
 from conversation import OllamaChat
 
-app = Flask(__name__, template_folder='templates')
 
-chat_bot = OllamaChat()
+class ChatBot:
+    def __init__(self):
+        self.chat_bot = OllamaChat()
+        self.app= Flask(self.__class__.__name__, template_folder='templates')
+        self._setup_routes()
 
-# Define route for homepage
-@app.route("/", methods=["GET", "POST"])
-def home():
-    response = ""
-    if request.method == "POST":
-        user_query = request.form.get("query")
-        chat_response = chat_bot.chat(user_query)
-        print("Chat Response:", chat_response['message']["content"])
-        response = f"Response: '{chat_response}'"
-    return render_template("base.html", response=response)
+
+    def _setup_routes(self):
+        @self.app.route("/", methods=["GET", "POST"])
+        def home():
+            response = ""
+            if request.method == "POST":
+                user_query = request.form.get("query")
+                chat_response = self.chat_bot.chat(user_query)
+                # print("Chat Response:", chat_response['message']["content"])
+                response = f"'{chat_response['message']['content']}'"
+            return render_template("base.html", response=response)
+
+    def run(self):
+        self.app.run(debug=True)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    chat_bot = ChatBot()
+    chat_bot.run()
